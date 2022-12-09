548929bf625675887681456c4c3b76337f1d4afd
amirhosseinolyaei <amirholyaei@gmail.com>
2022-12-09 11:35:16 -0500

    form

---
+++ val.js
@@ -0,0 +1,8 @@
+/validate the inputs for email and password and let the user know what they are missing
+The password has to be 8+ characters, with numbers, uppercase, lowercase, etc.
+You will be using regular expression and if-else statements.
+
+function val() {
+    document.getElementById("fname").value
+    
+}
